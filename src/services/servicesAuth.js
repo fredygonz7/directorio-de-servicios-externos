05af3eae360b5bc094268e63224d771444e8a828
FredyGonzalez <fredgonz7@gmail.com>
2021-08-29 15:42:54 -0500

    api-api

--- src/services/servicesAuth.js
+++ src/services/servicesAuth.js
@@ -1,26 +1,21 @@
-import axios from 'axios';
-import { directoryAPI } from '../api/directoryAPI';
+// import axios from 'axios';
+import { directoryAPI } from '../api/api/directoryAPI';
 
-const API_URL = 'http://localhost:8000/api/v1/auth';
+// const API_URL = 'http://localhost:8000/api/v1/auth';
 
-const servicesAuth = {
-    login: (infoUsuario) => {
-        return axios.post(`${API_URL}/login`, { ...infoUsuario })
-    },
-    register: (usuario) => {
-        // const config = {
-        //     headers: { Authorization: `Bearer ${token}` }
-        // };
-        return axios.post(`${API_URL}/auth/signup`, { ...usuario })
-    }
-}
+// const servicesAuth = {
+//     login: (infoUsuario) => {
+//         return axios.post(`${API_URL}/login`, { ...infoUsuario })
+//     },
+//     register: (usuario) => {
+//         // const config = {
+//         //     headers: { Authorization: `Bearer ${token}` }
+//         // };
+//         return axios.post(`${API_URL}/auth/signup`, { ...usuario })
+//     }
+// }
 
 const fetchAuthLogin = (data) => {
-    // localStorage.setItem("token", "access_token")
-    // const config = {
-    //     headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-    // };
-    // return jsonplaceholderApi.post(`/posts`, { ...data }, config)
     return directoryAPI.post(`/auth/login`, { ...data })
 }
 
@@ -29,9 +24,8 @@ const fetchAuthRegister = (data) => {
     const config = {
         headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
     };
-    // return jsonplaceholderApi.post(`/posts`, { ...data }, config)
     return directoryAPI.post(`/auth/register`, { ...data }, config)
 }
 */
 
-export { servicesAuth, fetchAuthLogin }
+export { fetchAuthLogin }
